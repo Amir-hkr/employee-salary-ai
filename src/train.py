7ed1b65a7e37b7eebcdf49846de28b2af642718a
amirabbas <amirabbashosseini0181@gmail.com>
2026-07-04 19:23:18 -0400

    added sklearn.model_selection

--- src/train.py
+++ src/train.py
@@ -24,3 +24,22 @@ df["Education Level"] = education_encoder.fit_transform(df["Education Level"])
 
 print("\nEncoded Dataset:")
 print(df.head())
+
+from sklearn.model_selection import train_test_split
+
+# Features (X)
+X = df.drop("Salary", axis=1)
+
+# Target (y)
+y = df["Salary"]
+
+# Split the dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+print("\nTraining Data Shape:", X_train.shape)
+print("Testing Data Shape:", X_test.shape)
